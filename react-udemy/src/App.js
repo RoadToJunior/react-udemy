73672eb3b84671d0effca1afbd818da7ef0de577
NowWeAre-Free <kontodarm@wp.pl>
2023-05-17 19:42:13 +0200

    forEach method repetition

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -6,13 +6,7 @@ function App() {
 
 export default App;
 
-const users = ["adam", "bogdan", "czarek", "darek"];
-
-const usersFirstLetterUpperCase = users.map((user) => user[0].toUpperCase());
-console.log(usersFirstLetterUpperCase);
-//["A", "B", "C", "D"];
-
-const numbers = [2, 4, 6];
-const doubleNumbers = numbers.map(number * 2);
-console.log(doubleNumbers);
-//[4, 8, 12];
+const usersAge = [20, 21, 23, 43];
+usersAge.forEach((age) =>
+  console.log(`W przyszłym roku użytkownik będzie miał ${age + 1} lat.`)
+);
