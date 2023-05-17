a6999b52c2352e0181f98de3027c2f1827de5ab7
NowWeAre-Free <kontodarm@wp.pl>
2023-05-17 19:45:00 +0200

    second example

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -7,6 +7,7 @@ function App() {
 export default App;
 
 const usersAge = [20, 21, 23, 43];
-usersAge.forEach((age) =>
-  console.log(`W przyszłym roku użytkownik będzie miał ${age + 1} lat.`)
-);
+let usersTotalAge = 0;
+usersAge.forEach((age) => (usersTotalAge += age));
+console.log(usersTotalAge);
+//107
