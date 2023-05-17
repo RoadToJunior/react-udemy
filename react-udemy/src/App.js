42e18916485c94a548cbf5215aa20fd8f0f18710
NowWeAre-Free <kontodarm@wp.pl>
2023-05-17 19:40:05 +0200

    second example

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -11,3 +11,8 @@ const users = ["adam", "bogdan", "czarek", "darek"];
 const usersFirstLetterUpperCase = users.map((user) => user[0].toUpperCase());
 console.log(usersFirstLetterUpperCase);
 //["A", "B", "C", "D"];
+
+const numbers = [2, 4, 6];
+const doubleNumbers = numbers.map(number * 2);
+console.log(doubleNumbers);
+//[4, 8, 12];
