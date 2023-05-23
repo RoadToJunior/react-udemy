3442e5ef4432bea139ef02cdff7c9ae2f948067e
NowWeAre-Free <kontodarm@wp.pl>
2023-05-23 21:42:27 +0200

    first app steps

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -1,41 +1,16 @@
 import React from "react";
 import "./App.css";
 
-class ShoppingList extends React.Component {
-  state = {
-    item1: "ogórki",
-    item2: "pomidory",
-    item3: "sałata",
-  };
+const handleClick = () => {};
 
+class App extends React.Component {
   render() {
     return (
-      <ul>
-        <li>
-          <ItemList name={this.state.item1} example={2 + 2} />
-          <ItemList name={this.state.item2} />
-          <ItemList name={this.state.item3} />
-        </li>
-      </ul>
+      <>
+        <button onClick={handleClick}>Add "a"</button>
+        <p></p>
+      </>
     );
   }
 }
-
-// const ItemList = (props) => {
-//   return (
-//     <li>
-//       {props.name} - {props.example}
-//     </li>
-//   );
-// };
-
-class ItemList extends React.Component {
-  render() {
-    return (
-      <li>
-        {this.props.name} - {this.props.example}
-      </li>
-    );
-  }
-}
-export default ShoppingList;
+export default App;
