50ec3eb107a16a8d0b8e3d84adb627f6f9b32e2b
NowWeAre-Free <kontodarm@wp.pl>
2023-05-25 19:18:57 +0200

    add function that display whatever you type in input

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    value: "",
+  };
+
   handleInputChange(e) {
-    console.log(e.target.value)
+    this.setState({
+      value: e.target.value,
+    });
   }
   render() {
     return (
       <>
-        <input onChange={handleInputChange} type="text"></input>
-        <button onClick={}>Reset</button>
-        <h1 className="title"></h1>
+        <input
+          placeholder="HIT A KEYBOARD"
+          onChange={this.handleInputChange.bind(this)}
+          type="text"
+        ></input>
+        {/* <button onClick={}>Reset</button> */}
+        <h1 className="title">{this.state.value}</h1>
       </>
     );
   }
