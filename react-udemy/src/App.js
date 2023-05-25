bfdb87ef80eaf21b9e3bd0e213a5451a7405f090
NowWeAre-Free <kontodarm@wp.pl>
2023-05-25 18:45:38 +0200

    init branch

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -1,28 +1,4 @@
 import React, { useState } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    text: "",
-  };
-  handleClick() {
-    const number = Math.floor(Math.random() * 10);
-
-    this.setState({
-      text: this.state.text + number,
-    });
-  }
-  render() {
-    return (
-      <>
-        <button onClick={this.handleClick.bind(this)}>Add "a"</button>
-        <PanelResault text={this.state.text} />
-      </>
-    );
-  }
-}
-
-const PanelResault = (props) => {
-  return <h1>{props.text}</h1>;
-};
 export default App;
