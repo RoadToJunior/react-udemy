e1f34ea575f476536bfe5ad1581b0fbca58f01ac
NowWeAre-Free <kontodarm@wp.pl>
2023-05-25 18:49:41 +0200

    add project structure

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -1,4 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
 
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <input type="text"></input>
+        <button>Reset</button>
+        <h1></h1>
+      </>
+    );
+  }
+}
 export default App;
