0043643e8101376de9a65b27a1f8ac64904885c6
NowWeAre-Free <kontodarm@wp.pl>
2023-05-26 21:49:26 +0200

    this practice

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    buttonName: ""
-  };
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageIsActive: false,
+    };
+  }
 
-  const mainPanel = () => {
-    render() {
-      return (
-        <>
-          <button>{this.state.buttonName}</button>
-        </>
-      )
-    }
-  };
+  handleClick() {
+    this.setState({
+      messageIsActive: true,
+    });
+  }
+
+  render() {
+    const text =
+      "lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa";
+
+    return (
+      <>
+        <button onClick={this.handleClick}>Click</button>
+        <p>{text}</p>
+      </>
+    );
+  }
 }
 
-export default App;
+export default Message;
