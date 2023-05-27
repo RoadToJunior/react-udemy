e149b8f0f39da5b8caafd315a6f977c27ebf9313
NowWeAre-Free <kontodarm@wp.pl>
2023-05-27 13:02:33 +0200

    show/hide app

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -7,22 +7,25 @@ class Message extends React.Component {
     this.state = {
       messageIsActive: false,
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     this.setState({
-      messageIsActive: true,
+      messageIsActive: !this.state.messageIsActive,
     });
   }
 
   render() {
     const text =
-      "lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa";
+      "lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa";
 
     return (
       <>
-        <button onClick={this.handleClick}>Click</button>
-        <p>{text}</p>
+        <button onClick={this.handleClick}>
+          {this.state.messageIsActive ? "Ukryj" : "Pokaż"}
+        </button>
+        {this.state.messageIsActive ? <p>{text}</p> : null}
       </>
     );
   }
