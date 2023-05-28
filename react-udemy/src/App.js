7502fbfb65f49b4826b6e6ebaa9805a5e68e14eb
NowWeAre-Free <kontodarm@wp.pl>
2023-05-28 09:27:46 +0200

    app structure

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -1,36 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
 
-class Message extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messageIsActive: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    debugger;
-    this.setState((prevState) => ({
-      messageIsActive: !prevState.messageIsActive,
-    }));
-  }
-
+class Counter extends React.Component {
+  state = {
+    count: 0,
+    result: 0,
+  };
   render() {
-    const text =
-      "lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa lorem ipsum dolor sit amet cons adici elit. Ateier mnet volus temporus sccotns moetase cisic visa";
-
     return (
       <>
-        <button onClick={this.handleClick}>
-          {this.state.messageIsActive ? "Ukryj" : "Pokaż"}
-        </button>
-        {/* {this.state.messageIsActive ? <p>{text}</p> : null} */}
-        {this.state.messageIsActive && <p>{text}</p>}
+        <button>-10</button>
+        <button>-1</button>
+        <button>Reset</button>
+        <button>+1</button>
+        <button>+10</button>
       </>
     );
   }
 }
-
-export default Message;
+export default Counter;
