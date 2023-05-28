fb51a0216d69980095e692ca8f3261c72e549c39
NowWeAre-Free <kontodarm@wp.pl>
2023-05-28 20:38:48 +0200

    add func component

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -28,11 +28,20 @@ class Counter extends React.Component {
   render() {
     return (
       <>
-        <MathButton />
+        <MathButton
+          name="-10"
+          number="10"
+          type="subtraction"
+          click={this.handleMathClick}
+        />
         <h1>Liczba kliknięć: {this.state.count}</h1>
         <h1>Wynik: {this.state.result}</h1>
       </>
     );
   }
 }
+const MathButton = (props) => {
+  <button onClick={() => props.click()}>{props.name}</button>;
+};
+
 export default Counter;
