fc33a2defba3de592d3a83c090c38348985dcd3c
NowWeAre-Free <kontodarm@wp.pl>
2023-05-28 09:34:11 +0200

    add functionality to first substraction button

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -6,14 +6,27 @@ class Counter extends React.Component {
     count: 0,
     result: 0,
   };
+  handleMathClick(type, number = 1) {
+    if (type === "subtraction") {
+      this.setState({
+        count: this.state.count + 1,
+        result: this.state.result - number,
+      });
+    }
+  }
+
   render() {
     return (
       <>
-        <button>-10</button>
+        <button onClick={this.handleMathClick.bind(this, "subtraction", 10)}>
+          -10
+        </button>
         <button>-1</button>
         <button>Reset</button>
         <button>+1</button>
         <button>+10</button>
+        <h1>Liczba kliknięć: {this.state.count} </h1>
+        <h1>Wynik: {this.state.result}</h1>
       </>
     );
   }
